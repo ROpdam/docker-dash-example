3a6f37091db0e44e4a89e2528304aa5956f23611
ROpdam <robinopdam@hotmail.com>
2021-04-11 21:12:53 +0200

    pre-commit-hook

--- project/app/functions.py
+++ project/app/functions.py
@@ -1,19 +1,22 @@
 import numpy as np
-from sklearn import linear_model #import LinearRegression
+from sklearn import linear_model
 import plotly.graph_objects as go
+
 
 _data_size = np.random.randint(100, 600)
 
+
 def plot_regression(std=10):
-    _x_og = np.arange(0,_data_size)
+    _x_og = np.arange(0, _data_size)
     x = _x_og.reshape((-1, 1))
-    y = _x_og * (np.full(shape=_data_size, fill_value=10, dtype=np.int) + np.random.normal(size=_data_size, loc=10, scale=std))
+    y = _x_og * (np.full(shape=_data_size, fill_value=10, dtype=np.int) +
+                 np.random.normal(size=_data_size, loc=10, scale=std))
 
     model = linear_model.LinearRegression().fit(x, y)
     r_sq = model.score(x, y)
     preds = model.predict(x)
 
-    layout  = go.Layout(title=f'What if you change standard deviation?\nR squared: {round(r_sq, 3)}', height=700)
+    layout = go.Layout(title=f'What if you change standard deviation?\nR squared: {round(r_sq, 3)}', height=700)
     fig = go.Figure(layout=layout)
 
     fig.add_trace(go.Scatter(x=_x_og, y=y, mode='markers', name=f'x * 10 + rand_norm(mean=10, std={std})'))

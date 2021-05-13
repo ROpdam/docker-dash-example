05c34eeb3b7005054883f766346b454a8f02e157
ROpdam <robinopdam@hotmail.com>
2021-05-13 17:18:06 +0200

    added isort pre-commit, updated readme

--- project/app/app.py
+++ project/app/app.py
@@ -1,11 +1,9 @@
 import dash
-
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 import flask
-
+from dash.dependencies import Input, Output
 from functions import plot_regression
 
 server = flask.Flask(__name__)

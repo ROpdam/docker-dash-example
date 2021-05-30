f826e55cb82eeab076f96bfd28bd4aadf7d773e1
ROpdam <robinopdam@hotmail.com>
2021-05-30 15:09:11 +0200

    using standard server from Dash

--- project/app/app.py
+++ project/app/app.py
@@ -2,13 +2,10 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-import flask
 from dash.dependencies import Input, Output
 from functions import plot_regression
 
-app = dash.Dash(
-    __name__, external_stylesheets=[dbc.themes.BOOTSTRAP], server=flask.Flask(__name__)
-)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 app.layout = html.Div(
